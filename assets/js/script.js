3dad9caccf340a0d83b1197ff1ea9c36b46733ed
Golnaz8 <golnaz.brj@gmail.com>
2023-06-19 16:38:23 -0400

    added Timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
 var startEl = document.querySelector("#start");
 var mainEl = document.querySelector(".main");
+var timerEl = document.querySelector("#timer");
+var questions = ["Q1", "Q2", "Q3", "Q4"];
 
 function firstQ() {
    mainEl.remove();
+   setTime();
+   
 
 }
-
 startEl.addEventListener("click",firstQ);
+
+var secondsLeft = 40;
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timerEl.textContent = `${secondsLeft} seconds left`;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+    }, 1000);
+  }
+  
