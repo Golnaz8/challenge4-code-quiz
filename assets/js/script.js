08f223aea9c9c633e0be4290498f7c02b2606d2b
Golnaz8 <golnaz.brj@gmail.com>
2023-06-19 16:19:02 -0400

    added first html,css,script files

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var startEl = document.querySelector("#start");
+var mainEl = document.querySelector(".main");
+
+function firstQ() {
+   mainEl.remove();
+
+}
+
+startEl.addEventListener("click",firstQ);
